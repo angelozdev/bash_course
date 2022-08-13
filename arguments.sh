8727b0cd30a8f9417ce20289ff4d7d5160b79836
Angelo Zambrano <angelozam17@gmail.com>
2022-08-13 15:25:21 -0500

    :sparkles: learning about arguments and variables

---
+++ arguments.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+
+echo "Numbre:           " $0
+echo "Argumento 1:      " $1
+echo "Contador de args: " $#
+echo "Todos los args:   " $*
---
+++ hello.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+age=24
+name="Angelo Zambrano"
+echo $UBI_REPOS
+echo "Hello, $name!"
+
+export name
+
+./hello_2
