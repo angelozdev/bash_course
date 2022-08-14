7dbd62a950b4bc879b39ce0114b632dfa9e6b645
Angelo Zambrano <angelozam17@gmail.com>
2022-08-13 19:10:19 -0500

    :recycle: update pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,5 +2,9 @@
 
 modified_files=$(git diff origin/main --name-only)
 
+if [[ -z $modified_files ]]; then
+	echo "No hay archivos :)"
+	exit 0
+fi
 
 echo "THESE FILES WERE MODIFIED: "$modified_files
